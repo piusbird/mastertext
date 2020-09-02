beaf682ce1c68e9407f85335964a90041b5b4f84
Matt Arnold <matt@thegnuguru.org>
2020-09-02 17:43:45 -0400

    Bang on frontend some more

--- mt.py
+++ mt.py
@@ -13,7 +13,8 @@ def cli():
 @cli.command()
 @click.argument('hash')
 @click.option('--attribs', default=False, help='Display Extended Attributes')
-def get(hash, attribs):
+@click.option('--less', default=False, help='Use the pager')
+def get(hash, attribs, less):
     """
     Retrevie a document object from the database by it's
     hashid.
@@ -23,17 +24,22 @@ def get(hash, attribs):
         click.echo("Attributes are not supported yet")
         click.echo(tso)
     else:
-        click.echo(tso)
+        if less:
+            click.echo_via_pager(tso)
+        else:
+            click.echo(tso)
 
 @cli.command()
 @click.argument('fts5term')
-def search(fts5term):
+@click.option('--less', default=False, help='Use the pager')
+def search(fts5term,less):
     search_id = ts.search_text(fts5term)
-    click.echo(search_id)
+    echo_function = click.echo_via_pager if less else click.echo
     il = search_id['ids']
+    echo_function("Results for: " + fts5term)
     for i in il:
-        click.echo(i)
-    click.echo(search_id['count'])
+        echo_function(i)
+    echo_function("Total = " + str(search_id['count']))
 
 if __name__ == '__main__':
     cli()
