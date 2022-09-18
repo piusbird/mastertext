7d149cbf19dd6ae735dbd7b103c7f0751446d3ca
Matt Arnold <matt@thegnuguru.org>
2022-09-17 21:54:52 -0400

    Several features coming down the pipe I know this is a bad commit but the rest will be in branches

--- mastertext/webfrontend/routes.py
+++ mastertext/webfrontend/routes.py
@@ -1,5 +1,5 @@
 from mastertext.webfrontend import app
-from flask import render_template, request
+from flask import render_template, request, flash, redirect
 from mastertext.objectstore import TextObjectStore, valid_hash, ObjectNotFoundError
 from mastertext.webfrontend import queries
 from mastertext.webfrontend import forms
@@ -48,3 +48,11 @@ def search_result():
     return render_template('results.html',
                            title="Search Results for: " + meta["query"], srs=q, metadata=meta)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
